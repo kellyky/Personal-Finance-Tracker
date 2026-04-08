9a2986b77dbc2acbfabe9f2e34a23edba2aa084f
cvalencia1991 <cesar4a6z@gmail.com>
2026-04-08 13:55:20 -0500

    Setup the routes for the login and signup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { SignupPage } from './pages/signup-page/signup-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginPage },
+  { path: 'signUp', component: SignupPage },
+];
