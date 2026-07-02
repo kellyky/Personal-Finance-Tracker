03a1f5b358b56c33f1e6d22a258a37c4e08dfa69
Kelly Popko <kelpopko@gmail.com>
2026-07-01 21:40:34 -0500

    Configure SimpleCov

--- backend/config/boot.rb
+++ backend/config/boot.rb
@@ -1,4 +1,11 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+if ENV["RAILS_ENV"] == "test" || (ENV["RAILS_ENV"].nil? && ARGV.any? { |arg| arg == "test" || arg == "t" })
+  require "simplecov"
+  SimpleCov.start "rails"
+end
+
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -1,3 +1,6 @@
+require "simplecov"
+SimpleCov.start 'rails'
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -10,6 +13,12 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
+    # Ensure each parallel worker has a unique SimpleCov command name to avoid overwriting data
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
+
     # Add more helper methods to be used by all tests here...
   end
 end
+
